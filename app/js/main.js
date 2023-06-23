454f8e114166aee1fb2b1de4627224b202adaca7
maksimkedys <maksimkedys@icloud.com>
2023-06-23 15:27:32 +0300

    added hero and popular sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function(){
-  
+$(function () {
+  var mixer = mixitup('.burger-catalog', {
+    load: {
+      filter: '.burger',
+    },
+    "animation": {
+      "duration": 500,
+      "nudge": false,
+      "reverseOut": true,
+      "effects": "fade"
+    }
+  });
 });
