20eb99adaa5caed0907b1eb25440f0b774fd4a99
maksimkedys <maksimkedys@icloud.com>
2023-06-30 19:16:07 +0300

    add sprite

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const uglify       = require('gulp-uglify');
 const imagemin     = require('gulp-imagemin');
 const del          = require('del');
+const svgSprite    = require('gulp-svg-sprite');
+const cheerio      = require('gulp-cheerio');
+const replace      = require('gulp-replace');
 const browserSync  = require('browser-sync').create();
 
 function browsersync() {
@@ -15,6 +18,30 @@ function browsersync() {
     },
     notify: false
   })
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(cheerio({
+      run: ($) => {
+        $("[fill]").removeAttr("fill");
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: { xmlMode: true },
+    })
+    )
+    .pipe(replace('&gt;', '>'))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
 }
 
 function styles() {
@@ -71,6 +98,7 @@ function cleanDist() {
 }
 
 function watching() {
+  watch(['app/images/icons/*.svg'], svgSprites)
   watch(['app/scss/**/*.scss'], styles)
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
   watch(['app/**/*.html']).on('change', browserSync.reload)
@@ -81,7 +109,8 @@ exports.scripts     = scripts;
 exports.browsersync = browsersync;
 exports.watching    = watching;
 exports.images      = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist   = cleanDist ;
 
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
