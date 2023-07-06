c9455b455adf0653d4fbdb3d07bc3953e752c84a
maksimkedys <maksimkedys@icloud.com>
2023-07-06 14:06:48 +0300

    added 4 sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,23 @@
 $(function () {
+  
+  $('.testimonials__inner').slick({
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-icon" width="11" height="19"><use href="images/sprite.svg#prev-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slick-icon" width="11" height="19"><use href="images/sprite.svg#next-arrow"></use></svg></button>',
+    fade: true
+  });
+
+  $('.slick-arrow').on('click', function() {
+    $('.slick-arrow').removeClass('current');
+    $(this).addClass('current');
+  });
 
   $(window).on('scroll', function () {
     $('.header__container').toggleClass('header__container--scrolled', $(window).scrollTop() > 0);
   });
 
-  var mixer = mixitup('.popular__catalog', {
+  var mixer = mixitup('.popular__catalog', {  
     load: {
       filter: '.burger',
     },
